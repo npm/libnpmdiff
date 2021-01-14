8e185659dd329af875cedc7e799dd77a7687c130
Ruy Adorno <ruyadorno@hotmail.com>
2021-01-14 01:53:09 +0000

    read-package-files
    - refactor lib/read-package-files.js
    - added tests

---
+++ tap-snapshots/test-read-package-files.js-TAP.test.js
@@ -0,0 +1,18 @@
+/* IMPORTANT
+ * This snapshot file is auto-generated, but designed for humans.
+ * It should be checked into source control and tracked carefully.
+ * Re-generate by setting TAP_SNAPSHOT=1 and running tests.
+ * Make sure to inspect the output below.  Do not ignore changes!
+ */
+'use strict'
+exports[`test/read-package-files.js TAP read files from a package cwd > should return list of filenames 1`] = `
+index.js
+test.js
+package.json
+`
+
+exports[`test/read-package-files.js TAP read files from a package cwd > should return map of filenames to its contents 1`] = `
+a/index.js: true
+a/test.js: true
+a/package.json: true
+`
