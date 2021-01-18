82f2fdd7e4e5825499b265f02c7273cda33716a8
Ruy Adorno <ruyadorno@hotmail.com>
2021-01-18 00:22:47 -0500

    fix lint

--- index.js
+++ index.js
@@ -16,42 +16,41 @@ const diff = async (specs, opts) => {
     specs.b = specs.a
     specs.a = `file:${path}`
     aManifest = await pacote.manifest(specs.a, opts)
-  } else {
+  } else
     bManifest = await pacote.manifest(specs.b, opts)
-  }
 
   const versions = {
     a: aManifest.version,
-    b: bManifest.version
+    b: bManifest.version,
   }
 
   // fetches tarball using pacote
   const [a, b] = await Promise.all([
     pacote.tarball(aManifest._resolved, opts),
-    pacote.tarball(bManifest._resolved, opts)
+    pacote.tarball(bManifest._resolved, opts),
   ])
 
   // read all files
   // populates `files` and `refs`
   const {
     files,
-    refs
+    refs,
   } = await untar([
     {
       prefix: 'a/',
-      item: a
+      item: a,
     },
     {
       prefix: 'b/',
-      item: b
-    }
+      item: b,
+    },
   ], opts)
 
   return formatDiff({
     files,
     opts,
     refs,
-    versions
+    versions,
   })
 }
 
